2a12e1c3d16b83c69e22a612bf3f489915984c64
vinnie-hso <vinicius.oliveira@visionaespacial.com.br>
2023-10-19 13:38:16 -0300

    Fix: healthcheck controller

--- src/controllers/HealthcheckController.ts
+++ src/controllers/HealthcheckController.ts
@@ -1,10 +1,19 @@
 import { Request, Response } from "express";
+import { RendimentoMuncipioService } from "../services/RendimentoMunicipioService";
 
 export class HealthcheckController {
-    constructor() { }
+    rendimentoMunicipioService: RendimentoMuncipioService
 
-    public check(request: Request, response: Response) {
+    constructor(rendimentoMunicipioService = new RendimentoMuncipioService) {
+        this.rendimentoMunicipioService = rendimentoMunicipioService
+    }
+
+    public async check(request: Request, response: Response) {
         try {
+            const municipio = 4103602
+            const cultura = "soja"
+            await this.rendimentoMunicipioService.getRendimento5AnosByMunicipio(municipio, cultura)
+            
             return response.status(200).json({ message: "Serviço PreSeg - Produtividade está saudável"})
         } catch (error: any) {
             return response.status(500).json({ message: `Serviço PreSeg - Produtividade error: ${error.message}`})
