9f3e11d703dcc5b92c619febf6b739d5fe19ae43
vinnie-hso <vinicius.oliveira@visionaespacial.com.br>
2023-11-07 14:56:16 -0300

    feat: add custom error class

---
+++ src/errors/CustomError.ts
@@ -0,0 +1,14 @@
+import { CustomErrorContent } from "../ts";
+
+export abstract class CustomError extends Error {
+    abstract readonly statusCode: number;
+    abstract readonly errors: CustomErrorContent[];
+    abstract readonly logging: boolean;
+
+    constructor(message: string) {
+        super(message);
+
+        // Only because we are extending a built in class
+        Object.setPrototypeOf(this, CustomError.prototype);
+    }
+}
